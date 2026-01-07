b24133020e9fb5d82f15d5cd18dd02e777e1ee21
kitahara-6355 <hirobrandneo@gmail,com>
2026-01-07 13:00:49 +0900

    security: APIキーを含むファイルを削除

--- append_secrets.py
+++
@@ -1,7 +0,0 @@
-
-# Valid UTF-8 append for secrets
-content = '\nAPP_PASSWORD = "30bousai"'
-with open(".streamlit/secrets.toml", "a", encoding="utf-8") as f:
-    f.write(content)
-
-print(f"Successfully appended APP_PASSWORD.")
